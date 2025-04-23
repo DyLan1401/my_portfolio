be7d2d4b5b2b76e28319d1859c4db16ac3c0361b
Dylan <nlan4670@gmail.com>
2025-04-24 03:48:27 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my_portfolio/', // 👈 phải trùng tên repo
   plugins: [  tailwindcss(),react()],
 })
